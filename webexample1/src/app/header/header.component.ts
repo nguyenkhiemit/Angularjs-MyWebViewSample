e3d42f0e6ba7f120bed8cb51122a9c2918af2658
Khiem Nguyen <nguyenkhiemit@gmail.com>
2018-04-16 11:15:23 +0700

    create project

---
+++ webexample1/src/app/header/header.component.ts
@@ -0,0 +1,31 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-header',
+  templateUrl: './header.component.html',
+  styleUrls: ['./header.component.css']
+})
+export class HeaderComponent implements OnInit {
+
+  items: [any];
+
+  checked = true;
+
+  constructor() {
+    this.items = [
+      { 'currentIndex': true , 'path': 'home', 'name': 'HOME' },
+      { 'currentIndex': false , 'path': 'about', 'name': 'ABOUT' },
+      { 'currentIndex': false , 'path': 'service', 'name': 'SERVICE' },
+    ];
+  }
+
+  ngOnInit() {
+  }
+
+  changeIndex(index: number) {
+    for (let i = 0; i < this.items.length; i++) {
+      this.items[i].currentIndex = false;
+    }
+    this.items[index].currentIndex = true;
+  }
+}
